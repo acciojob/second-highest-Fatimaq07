7be445c3930ad3ad7f41410cf83380b07ccc5cc8
FATIMA QURESHI <115686917+Fatimaq07@users.noreply.github.com>
2025-08-11 14:09:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+  if (arr.length < 2) return -Infinity;
+  let max = Math.max(...arr);
+  let filtered = arr.filter(num => num !== max);
+  if (filtered.length === 0) return -Infinity;
+  return Math.max(...filtered);
+}
